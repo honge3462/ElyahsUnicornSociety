4281726aedd706a017aa05f364f5e4e55f5bc433
Ellie <ehong@developertown.com>
2020-08-21 19:46:02 -0400

    first commit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,10 @@
+import React, { FC } from "react";
+import pinkblob from "../../img/pinkblob.svg"
+
+const Header: FC = () => {
+    return (
+        <img src={pinkblob} />
+    )
+}
+
+export default Header;
